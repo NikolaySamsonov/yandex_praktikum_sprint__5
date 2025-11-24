7c46e6e0d1e85b231579759c46b099c9ccfd6cf4
NikolaySamsonov <65284383+NikolaySamsonov@users.noreply.github.com>
2025-11-24 04:04:26 +0300

    fix 2

--- go1fl-sprint5-final-main/internal/daysteps/daysteps.go
+++ go1fl-sprint5-final-main/internal/daysteps/daysteps.go
@@ -1,13 +1,62 @@
 package daysteps
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	Personal personaldata.Personal
 }
 
-func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+func (ds *DaySteps) Parse(datastring string) error {
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 2 {
+		return errors.New("неверный формат данных: должно быть 2 поля")
+	}
+
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return err
+	}
+	ds.Steps = steps
+	if steps <= 0 {
+		return errors.New("значение шагов должно быть больше 0")
+	}
+	d, err := time.ParseDuration(parts[1])
+	if err != nil {
+		return err
+	}
+	ds.Duration = d
+	if d <= 0 {
+		return errors.New("длительность тренировки должна быть больше 0")
+	}
+
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	dist := spentenergy.Distance(ds.Steps, ds.Personal.Height)
+
+	calories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Personal.Weight, ds.Personal.Height, ds.Duration)
+	if err != nil {
+		return "", err
+	}
+
+	result := fmt.Sprintf(
+		"Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
+		ds.Steps,
+		dist,
+		calories,
+	)
+
+	return result, nil
 }
--- go1fl-sprint5-final-main/internal/personaldata/personaldata.go
+++ go1fl-sprint5-final-main/internal/personaldata/personaldata.go
@@ -1,9 +1,25 @@
 package personaldata
 
+import (
+	"fmt"
+	"log"
+)
+
 type Personal struct {
-	// TODO: добавить поля
+	Name   string
+	Weight float64
+	Height float64
 }
 
 func (p Personal) Print() {
-	// TODO: реализовать функцию
+	msg := fmt.Sprintf(
+		"Имя: %s\nВес: %.2f кг.\nРост: %.2f м.\n",
+		p.Name,
+		p.Weight,
+		p.Height,
+	)
+
+	log.Println("PRINT:", msg)
+
+	fmt.Print(msg)
 }
